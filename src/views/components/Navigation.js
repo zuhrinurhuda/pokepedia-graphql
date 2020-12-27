374b4dfcab928986d1597937b4461dd6075260fa
Zuhri Nurhuda <zuhri.nurhuda@gmail.com>
2020-12-27 11:40:15 +0700

    Create navigation

---
+++ src/views/components/Navigation.js
@@ -0,0 +1,54 @@
+/** @jsxImportSource @emotion/react */
+import { css } from "@emotion/react";
+import { NavLink } from "react-router-dom";
+
+export const Navigation = (props) => (
+  <nav
+    css={css`
+      padding-bottom: 56px;
+    `}
+  >
+    <div
+      css={css`
+        height: 56px;
+        position: fixed;
+        bottom: 0px;
+        width: 100%;
+        background-color: rgb(255, 255, 255);
+        box-shadow: rgba(108, 114, 124, 0.16) 0px -2px 4px 0px;
+        display: flex;
+        max-width: 500px;
+      `}
+      {...props}
+    />
+  </nav>
+);
+
+export const NavigationItem = ({ to, width, ...props }) => (
+  <NavLink
+    css={css`
+      padding: 8px 0px;
+      display: flex;
+      align-items: center;
+      flex-flow: column nowrap;
+      font-size: 10px;
+      color: rgba(49, 53, 59, 0.68);
+      text-decoration: initial;
+      width: ${width};
+    `}
+    to={to}
+  >
+    <div
+      css={css`
+        display: contents;
+        &,
+        & > img {
+          width: 24px;
+          height: 24px;
+          margin-bottom: 4px;
+        }
+      `}
+      {...props}
+    />
+  </NavLink>
+);
--- src/views/layout/App.js
+++ src/views/layout/App.js
@@ -1,15 +1,38 @@
 import React from "react";
-import { Route } from "react-router-dom";
+import { Route, useLocation } from "react-router-dom";
 
+import { Container, Navigation, NavigationItem } from "views/components";
 import routes from "routes";
 
 function App() {
+  const location = useLocation();
+
   return (
-    <>
+    <Container>
       {routes.map((route) => (
         <Route key={route.path} {...route} />
       ))}
-    </>
+      <Navigation>
+        <NavigationItem width="50%" to="/">
+          <img
+            src={`https://assets.tokopedia.net/assets-tokopedia-lite/v2/atreus/kratos/${
+              location.pathname === "/" ? "811ee09c" : "518bf1c8"
+            }.svg`}
+            alt="home"
+          />
+          Pokedex
+        </NavigationItem>
+        <NavigationItem width="50%" to="/my-pokemon">
+          <img
+            src={`https://assets.tokopedia.net/assets-tokopedia-lite/v2/atreus/kratos/${
+              location.pathname === "/my-pokemon" ? "99a999cc" : "7187ed7c"
+            }.svg`}
+            alt="my pokemon list"
+          />
+          My Pokemon
+        </NavigationItem>
+      </Navigation>
+    </Container>
   );
 }
 
